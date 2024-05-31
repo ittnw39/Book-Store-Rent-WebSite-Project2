8fb403f7115a8d54a616b3f93233fb1469e25e6e
Hyung Woon Yoon <tommyhw97@gmail.com>
2024-05-31 22:56:07 +0900

    파일추가

---
+++ src/main/java/io/elice/shoppingmall/user/entity/UserController.java
@@ -0,0 +1,17 @@
+package io.elice.shoppingmall.user.entity;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+
+    private final UserRepository userRepository;
+
+
+
+
+}
